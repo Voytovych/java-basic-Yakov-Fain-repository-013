ebd210c5c5ad7df16d6d53f8e1256e2116b2f5bf
Voytovych <voytovych.ua@gmail.com>
2016-07-11 16:56:56 +0300

    Unit-07

---
+++ Unit07-TestStackTrace/src/com/voytovych/basic/TestStackTrace.java
@@ -0,0 +1,19 @@
+package com.voytovych.basic;
+
+class TestStackTrace{    
+   TestStackTrace()
+   {
+       divideByZero();
+   }
+
+    int divideByZero()
+    {
+        return 25/0;
+    }
+
+    public static void main(String[]args)
+    {
+           new TestStackTrace();
+
+    }
+}
