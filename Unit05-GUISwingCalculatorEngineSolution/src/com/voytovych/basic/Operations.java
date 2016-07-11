666a4e003b89803136ce7310cb967606f706f26a
Voytovych <voytovych.ua@gmail.com>
2016-07-11 11:19:35 +0300

    Unit-05

---
+++ Unit05-GUISwingCalculatorEngineSolution/src/com/voytovych/basic/Operations.java
@@ -0,0 +1,12 @@
+package com.voytovych.basic;
+
+/**
+ * Used to simplify the addition of a new operation
+ */
+public enum Operations {
+	ADDITION,
+	SUBTRACTION,
+	DIVISION,
+	MULTIPLYING,
+	ILLEGAL_OPERATION
+}
