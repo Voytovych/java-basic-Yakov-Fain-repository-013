743366d6e0ee6f21ea941d514374a4d3710c86c4
Voytovych <voytovych.ua@gmail.com>
2016-07-12 12:57:28 +0300

    Unit-08

---
+++ Unit08-Collections/src/com/voytovych/basic/Customer.java
@@ -0,0 +1,5 @@
+package com.voytovych.basic;
+
+public class Customer {
+    public Customer (String a, String b){}
+}
---
+++ Unit08-Collections/src/com/voytovych/basic/TestVariousCollections.java
@@ -0,0 +1,35 @@
+package com.voytovych.basic;
+
+import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
+public class TestVariousCollections {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		HashSet sss = new HashSet();
+		sss.add("Mary");
+		sss.add("Mary");
+			
+		ArrayList aaa = new ArrayList();
+		
+		aaa.add("Mary");
+		aaa.add("Mary");
+		
+		
+		Vector bbb = new Vector();
+		
+		
+		HashMap hm=new HashMap();
+		hm.put("customer", new Customer("John", "Smith")); 
+		hm.put("order", new Order());
+
+
+		
+		
+	}
+
+}
